99a522cf9709bd7bdddee9e4e1e4357b63a32cc7
Vibhav Singh <166403270+Vibhav-Singh@users.noreply.github.com>
2024-05-06 20:08:23 +0530

    Update auth.js 2

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-// Checking-again prompt changes May-6
+// Checking-again-2 prompt changes May-6
 import {User} from '../models/User';
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
