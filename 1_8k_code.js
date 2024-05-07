4f33cc5949af42440d9a3ad2ab1a56bfa9986fb0
Vibhav Singh <166403270+Vibhav-Singh@users.noreply.github.com>
2024-05-07 18:44:12 +0530

    Update 1_8k_code.js

--- 1_8k_code.js
+++ 1_8k_code.js
@@ -1,245 +1,336 @@
+'use strict';
 
+/* eslint-disable no-for-of-loops/no-for-of-loops */
 
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow
- */
+const getComments = require('./getComments');
 
-/* eslint-disable */
+function transform(babel) {
+  const {types: t} = babel;
 
-// libdefs cannot actually import. These are supposed to be the types imported
-// from 'react-native-renderer/src/ReactNativeTypes'
-type __MeasureOnSuccessCallback = any;
-type __MeasureInWindowOnSuccessCallback = any;
-type __MeasureLayoutOnSuccessCallback = any;
-type __ReactNativeBaseComponentViewConfig = any;
-type __ViewConfigGetter = any;
-type __ViewConfig = any;
+  // A very stupid subset of pseudo-JavaScript, used to run tests conditionally
+  // based on the environment.
+  //
+  // Input:
+  //   @gate a && (b || c)
+  //   test('some test', () => {/*...*/})
+  //
+  // Output:
+  //   @gate a && (b || c)
+  //   _test_gate(ctx => ctx.a && (ctx.b || ctx.c), 'some test', () => {/*...*/});
+  //
+  // expression     →  binary ( ( "||" | "&&" ) binary)* ;
+  // binary         →  unary ( ( "==" | "!=" | "===" | "!==" ) unary )* ;
+  // unary          →  "!" primary
+  //                |  primary ;
+  // primary        →  NAME | STRING | BOOLEAN
+  //                |  "(" expression ")" ;
+  function tokenize(code) {
+    const tokens = [];
+    let i = 0;
+    while (i < code.length) {
+      let char = code[i];
+      // Double quoted strings
+      if (char === '"') {
+        let string = '';
+        i++;
+        do {
+          if (i > code.length) {
+            throw Error('Missing a closing quote');
+          }
+          char = code[i++];
+          if (char === '"') {
+            break;
+          }
+          string += char;
+        } while (true);
+        tokens.push({type: 'string', value: string});
+        continue;
+      }
 
-// libdefs cannot actually import. This is supposed to be the type imported
-// from 'react-native-renderer/src/legacy-events/TopLevelEventTypes';
-type __RNTopLevelEventType = any;
+      // Single quoted strings
+      if (char === "'") {
+        let string = '';
+        i++;
+        do {
+          if (i > code.length) {
+            throw Error('Missing a closing quote');
+          }
+          char = code[i++];
+          if (char === "'") {
+            break;
+          }
+          string += char;
+        } while (true);
+        tokens.push({type: 'string', value: string});
+        continue;
+      }
 
-// libdefs cannot actually import. This is supposed to be the type imported
-// from 'react-reconciler/src/ReactCapturedValue'
-type __CapturedError = any;
+      // Whitespace
+      if (/\s/.test(char)) {
+        if (char === '\n') {
+          return tokens;
+        }
+        i++;
+        continue;
+      }
 
-type DeepDifferOptions = {+unsafelyIgnoreFunctions?: boolean};
-type RawEventEmitterEvent = $ReadOnly<{
-  eventName: string,
-  // We expect, but do not/cannot require, that nativeEvent is an object
-  // with the properties: key, elementType (string), type (string), tag (numeric),
-  // and a stateNode of the native element/Fiber the event was emitted to.
-  nativeEvent: {[string]: mixed, ...},
-}>;
+      const next3 = code.slice(i, i + 3);
+      if (next3 === '===') {
+        tokens.push({type: '=='});
+        i += 3;
+        continue;
+      }
+      if (next3 === '!==') {
+        tokens.push({type: '!='});
+        i += 3;
+        continue;
+      }
 
-declare module 'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface' {
-  declare export function deepDiffer(
-    one: any,
-    two: any,
-    maxDepth?: number,
-    options?: DeepDifferOptions,
-  ): boolean;
-  declare export function deepDiffer(
-    one: any,
-    two: any,
-    options: DeepDifferOptions,
-  ): boolean;
-  declare export function deepFreezeAndThrowOnMutationInDev<T>(obj: T): T;
-  declare export function flattenStyle(style: any): any;
-  declare export const RCTEventEmitter: {
-    register: (eventEmitter: mixed) => void,
-    ...
-  };
-  declare export const TextInputState: {
-    blurTextInput: (object: any) => void,
-    focusTextInput: (object: any) => void,
-    ...
-  };
-  declare export const ReactFiberErrorDialog: {
-    showErrorDialog: (error: __CapturedError) => boolean,
-    ...
-  };
-  declare export const Platform: {OS: string, ...};
-  declare export const UIManager: {
-    customBubblingEventTypes: Object,
-    customDirectEventTypes: Object,
-    createView: (
-      reactTag: number,
-      viewName: string,
-      rootTag: number,
-      props: ?Object,
-    ) => void,
-    dispatchViewManagerCommand: (
-      reactTag: number,
-      command: string,
-      args: Array<any>,
-    ) => void,
-    manageChildren: (
-      containerTag: number,
-      moveFromIndices: Array<number>,
-      moveToIndices: Array<number>,
-      addChildReactTags: Array<number>,
-      addAtIndices: Array<number>,
-      removeAtIndices: Array<number>,
-    ) => void,
-    measure: (hostComponent: mixed, callback: Function) => void,
-    measureInWindow: (nativeTag: ?number, callback: Function) => void,
-    measureLayout: (
-      nativeTag: mixed,
-      nativeNode: number,
-      onFail: Function,
-      onSuccess: Function,
-    ) => void,
-    removeRootView: (containerTag: number) => void,
-    removeSubviewsFromContainerWithID: (containerId: number) => void,
-    replaceExistingNonRootView: () => void,
-    setChildren: (containerTag: number, reactTags: Array<number>) => void,
-    updateView: (reactTag: number, viewName: string, props: ?Object) => void,
-    __takeSnapshot: (
-      view?: 'window' | Element<any> | number,
-      options?: {
-        width?: number,
-        height?: number,
-        format?: 'png' | 'jpeg',
-        quality?: number,
-        ...
+      const next2 = code.slice(i, i + 2);
+      switch (next2) {
+        case '&&':
+        case '||':
+        case '==':
+        case '!=':
+          tokens.push({type: next2});
+          i += 2;
+          continue;
+        case '//':
+          // This is the beginning of a line comment. The rest of the line
+          // is ignored.
+          return tokens;
+      }
+
+      switch (char) {
+        case '(':
+        case ')':
+        case '!':
+          tokens.push({type: char});
+          i++;
+          continue;
+      }
+
+      // Names
+      const nameRegex = /[a-zA-Z_$][0-9a-zA-Z_$]*/y;
+      nameRegex.lastIndex = i;
+      const match = nameRegex.exec(code);
+      if (match !== null) {
+        const name = match[0];
+        switch (name) {
+          case 'true': {
+            tokens.push({type: 'boolean', value: true});
+            break;
+          }
+          case 'false': {
+            tokens.push({type: 'boolean', value: false});
+            break;
+          }
+          default: {
+            tokens.push({type: 'name', name});
+          }
+        }
+        i += name.length;
+        continue;
+      }
+
+      throw Error('Invalid character: ' + char);
+    }
+    return tokens;
+  }
+
+  function parse(code, ctxIdentifier) {
+    const tokens = tokenize(code);
+
+    let i = 0;
+    function parseExpression() {
+      let left = parseBinary();
+      while (true) {
+        const token = tokens[i];
+        if (token !== undefined) {
+          switch (token.type) {
+            case '||':
+            case '&&': {
+              i++;
+              const right = parseBinary();
+              if (right === null) {
+                throw Error('Missing expression after ' + token.type);
+              }
+              left = t.logicalExpression(token.type, left, right);
+              continue;
+            }
+          }
+        }
+        break;
+      }
+      return left;
+    }
+
+    function parseBinary() {
+      let left = parseUnary();
+      while (true) {
+        const token = tokens[i];
+        if (token !== undefined) {
+          switch (token.type) {
+            case '==':
+            case '!=': {
+              i++;
+              const right = parseUnary();
+              if (right === null) {
+                throw Error('Missing expression after ' + token.type);
+              }
+              left = t.binaryExpression(token.type, left, right);
+              continue;
+            }
+          }
+        }
+        break;
+      }
+      return left;
+    }
+
+    function parseUnary() {
+      const token = tokens[i];
+      if (token !== undefined) {
+        if (token.type === '!') {
+          i++;
+          const argument = parseUnary();
+          return t.unaryExpression('!', argument);
+        }
+      }
+      return parsePrimary();
+    }
+
+    function parsePrimary() {
+      const token = tokens[i];
+      switch (token.type) {
+        case 'boolean': {
+          i++;
+          return t.booleanLiteral(token.value);
+        }
+        case 'name': {
+          i++;
+          return t.memberExpression(ctxIdentifier, t.identifier(token.name));
+        }
+        case 'string': {
+          i++;
+          return t.stringLiteral(token.value);
+        }
+        case '(': {
+          i++;
+          const expression = parseExpression();
+          const closingParen = tokens[i];
+          if (closingParen === undefined || closingParen.type !== ')') {
+            throw Error('Expected closing )');
+          }
+          i++;
+          return expression;
+        }
+        default: {
+          throw Error('Unexpected token: ' + token.type);
+        }
+      }
+    }
+
+    const program = parseExpression();
+    if (tokens[i] !== undefined) {
+      throw Error('Unexpected token');
+    }
+    return program;
+  }
+
+  function buildGateCondition(comments) {
+    let conditions = null;
+    for (const line of comments) {
+      const commentStr = line.value.trim();
+      if (commentStr.startsWith('@gate ')) {
+        const code = commentStr.slice(6);
+        const ctxIdentifier = t.identifier('ctx');
+        const condition = parse(code, ctxIdentifier);
+        if (conditions === null) {
+          conditions = [condition];
+        } else {
+          conditions.push(condition);
+        }
+      }
+    }
+    if (conditions !== null) {
+      let condition = conditions[0];
+      for (let i = 1; i < conditions.length; i++) {
+        const right = conditions[i];
+        condition = t.logicalExpression('&&', condition, right);
+      }
+      return condition;
+    } else {
+      return null;
+    }
+  }
+
+  return {
+    name: 'test-gate-pragma',
+    visitor: {
+      ExpressionStatement(path) {
+        const statement = path.node;
+        const expression = statement.expression;
+        if (expression.type === 'CallExpression') {
+          const callee = expression.callee;
+          switch (callee.type) {
+            case 'Identifier': {
+              if (
+                callee.name === 'test' ||
+                callee.name === 'it' ||
+                callee.name === 'fit'
+              ) {
+                const comments = getComments(path);
+                if (comments !== undefined) {
+                  const condition = buildGateCondition(comments);
+                  if (condition !== null) {
+                    callee.name =
+                      callee.name === 'fit' ? '_test_gate_focus' : '_test_gate';
+                    expression.arguments = [
+                      t.arrowFunctionExpression(
+                        [t.identifier('ctx')],
+                        condition
+                      ),
+                      ...expression.arguments,
+                    ];
+                  }
+                }
+              }
+              break;
+            }
+            case 'MemberExpression': {
+              if (
+                callee.object.type === 'Identifier' &&
+                (callee.object.name === 'test' ||
+                  callee.object.name === 'it') &&
+                callee.property.type === 'Identifier' &&
+                callee.property.name === 'only'
+              ) {
+                const comments = getComments(path);
+                if (comments !== undefined) {
+                  const condition = buildGateCondition(comments);
+                  if (condition !== null) {
+                    statement.expression = t.callExpression(
+                      t.identifier('_test_gate_focus'),
+                      [
+                        t.arrowFunctionExpression(
+                          [t.identifier('ctx')],
+                          condition
+                        ),
+                        ...expression.arguments,
+                      ]
+                    );
+                  }
+                }
+              }
+              break;
+            }
+          }
+        }
+        return;
       },
-    ) => Promise<any>,
-    setJSResponder: (reactTag: number, blockNativeResponder: boolean) => void,
-    clearJSResponder: () => void,
-    findSubviewIn: (
-      reactTag: ?number,
-      point: Array<number>,
-      callback: (
-        nativeViewTag: number,
-        left: number,
-        top: number,
-        width: number,
-        height: number,
-      ) => void,
-    ) => void,
-    ...
+    },
   };
-  declare export const legacySendAccessibilityEvent: (
-    reactTag: number,
-    eventTypeName: string,
-  ) => void;
-  declare export const BatchedBridge: {
-    registerCallableModule: (name: string, module: Object) => void,
-    ...
-  };
-  declare export const ReactNativeViewConfigRegistry: {
-    customBubblingEventTypes: Object,
-    customDirectEventTypes: Object,
-
-    register: (name: string, callback: __ViewConfigGetter) => string,
-    get: (name: string) => __ReactNativeBaseComponentViewConfig,
-    ...
-  };
-  declare export const RawEventEmitter: {
-    emit: (channel: string, event: RawEventEmitterEvent) => string,
-    ...
-  };
-  declare export opaque type PublicInstance;
-  declare export opaque type PublicTextInstance;
-  declare export function getNodeFromPublicInstance(
-    publicInstance: PublicInstance,
-  ): Object;
-  declare export function getNativeTagFromPublicInstance(
-    publicInstance: PublicInstance,
-  ): number;
-  declare export function createPublicInstance(
-    tag: number,
-    viewConfig: __ViewConfig,
-    internalInstanceHandle: mixed,
-  ): PublicInstance;
-  declare export function createPublicTextInstance(
-    internalInstanceHandle: mixed,
-  ): PublicTextInstance;
-  declare export function getInternalInstanceHandleFromPublicInstance(
-    publicInstance: PublicInstance,
-  ): ?Object;
 }
 
-declare module 'react-native/Libraries/ReactPrivate/ReactNativePrivateInitializeCore' {
-}
-
-declare const RN$enableMicrotasksInReact: boolean;
-
-// This is needed for a short term solution.
-// See https://github.com/facebook/react/pull/15490 for more info
-declare const nativeFabricUIManager: {
-  createNode: (
-    reactTag: number,
-    viewName: string,
-    rootTag: number,
-    props: ?Object,
-    eventTarget: Object,
-  ) => Object,
-  cloneNode: (node: Object) => Object,
-  cloneNodeWithNewChildren: (
-    node: Object,
-    children?: $ReadOnlyArray<Object>,
-  ) => Object,
-  cloneNodeWithNewProps: (node: Object, newProps: ?Object) => Object,
-  cloneNodeWithNewChildrenAndProps: (
-    node: Object,
-    newPropsOrChildren: ?Object | $ReadOnlyArray<Object>,
-    newProps?: ?Object,
-  ) => Object,
-  appendChild: (node: Object, childNode: Object) => void,
-
-  createChildSet: () => Object,
-  appendChildToSet: (childSet: Object, childNode: Object) => void,
-  completeRoot: (rootTag: number, childSet: Object) => void,
-  registerEventHandler: (
-    callback: (
-      eventTarget: null | Object,
-      type: __RNTopLevelEventType,
-      payload: Object,
-    ) => void,
-  ) => void,
-  setNativeProps: (node: Object, nativeProps: Object) => Object,
-  dispatchCommand: (node: Object, command: string, args: Array<any>) => void,
-  sendAccessibilityEvent: (node: Object, eventTypeName: string) => void,
-
-  measure: (node: Object, callback: __MeasureOnSuccessCallback) => void,
-  measureInWindow: (
-    node: Object,
-    callback: __MeasureInWindowOnSuccessCallback,
-  ) => void,
-  measureLayout: (
-    node: Object,
-    relativeNode: Object,
-    onFail: () => void,
-    onSuccess: __MeasureLayoutOnSuccessCallback,
-  ) => void,
-  getBoundingClientRect: (
-    node: Object,
-  ) => [
-    /* x:*/ number,
-    /* y:*/ number,
-    /* width:*/ number,
-    /* height:*/ number,
-  ],
-  findNodeAtPoint: (
-    node: Object,
-    locationX: number,
-    locationY: number,
-    callback: (Object) => void,
-  ) => void,
-  setIsJSResponder: (
-    node: Object,
-    isJsResponder: boolean,
-    blockNativeResponder: boolean,
-  ) => void,
-  unstable_DefaultEventPriority: number,
-  unstable_DiscreteEventPriority: number,
-  unstable_getCurrentEventPriority: () => number,
-  ...
-};
+module.exports = transform;
