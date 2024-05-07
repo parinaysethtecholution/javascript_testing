db2cd86a199890363b16c22da5bea3436b53041c
Vibhav Singh <166403270+Vibhav-Singh@users.noreply.github.com>
2024-05-07 17:06:13 +0530

    Update long_code.js

--- long_code.js
+++ long_code.js
@@ -1,6 +1,7 @@
 // This is a sample JavaScript code that is long enough to exceed 2000 tokens when tokenized.
 // Just for checking if long scripts are getting successfully refactored or not.
 // PR didn't get raised. Trying again to make sure.
+// Trying again
 // Define a function that generates a random token
 function generateToken(length) {
     let result = '';
