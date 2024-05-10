a2710426d3ad89133c2d8a86891cb6ec9a3913ad
Harshit Rathore <harshit.rathore@techolution.com>
2024-05-10 09:33:06 +0000

    Refactored code updated

--- customHook.js
+++ customHook.js
@@ -1,41 +1,58 @@
+
 // For testing Unit Test Code Generation
 
-const { useEffect } = require("react");
-const { useDispatch, useSelector } = require("react-redux");
-const { apiOptions, movieVideoUrl } = require("../Constants/apiConstants");
-const { setMovieTrailer } = require("../store/moviesSlice");
-const { isEmpty } = require("lodash");
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { apiOptions, movieVideoUrl } from "../Constants/apiConstants";
+import { setMovieTrailer } from "../store/moviesSlice";
+import { isEmpty } from "lodash";
 
-
+/**
+ * Custom hook to fetch and store the movie trailer in the Redux store.
+ *
+ * @param {number} movieId - The ID of the movie for which to fetch the trailer.
+ */
 const useFetchMovieTrailer = (movieId) => {
-    const dispatch = useDispatch();
-    const trailer = useSelector(store => store?.movie?.movieTrailer);
-    const fetchTrailer = async () => {
-        const apiData = await fetch(movieVideoUrl(movieId), apiOptions);
-        const jsonData = await apiData.json();
-        let trailers;
-        if (jsonData && jsonData.results) {
-            trailers = jsonData.results.filter((elem) => {
-                if (elem && elem.type === "Trailer") {
-                    return true;
-                }
-                return false;
-            });
+  const dispatch = useDispatch();
+  const trailer = useSelector((store) => store?.movie?.movieTrailer);
+
+  /**
+   * Fetches the movie trailer from the API and dispatches it to the Redux store.
+   */
+  const fetchTrailer = async () => {
+    const apiData = await fetch(movieVideoUrl(movieId), apiOptions);
+    const jsonData = await apiData.json();
+    let trailers;
+
+    // Filter the API response to get only the trailer videos
+    if (jsonData && jsonData.results) {
+      trailers = jsonData.results.filter((elem) => {
+        if (elem && elem.type === "Trailer") {
+          return true;
         }
-        let trailerObject;
-        if (trailers && trailers.length > 0) {
-            trailerObject = trailers[0];
-        } else {
-            trailerObject = jsonData.results[0];
-        }
-        dispatch(setMovieTrailer(trailerObject))
+        return false;
+      });
     }
 
-    useEffect(() => {
-        if (isEmpty(trailer)) {
-            fetchTrailer()
-        }
-    }, [])
-}
+    let trailerObject;
+
+    // If there are trailers, use the first one; otherwise, use the first result
+    if (trailers && trailers.length > 0) {
+      trailerObject = trailers[0];
+    } else {
+      trailerObject = jsonData.results[0];
+    }
+
+    // Dispatch the trailer object to the Redux store
+    dispatch(setMovieTrailer(trailerObject));
+  };
+
+  // Fetch the trailer if it's not already in the store
+  useEffect(() => {
+    if (isEmpty(trailer)) {
+      fetchTrailer();
+    }
+  }, []);
+};
 
 export default useFetchMovieTrailer;
