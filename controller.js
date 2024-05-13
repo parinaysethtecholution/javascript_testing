ec9815c7f8cb2850f8701a67240bbecc30f6e321
Harshit Rathore <harshit.rathore@techolution.com>
2024-05-13 15:33:43 +0000

    Refactored code updated

--- controller.js
+++ controller.js
@@ -1,29 +1,36 @@
-var Product = require('../models/Product');
+
+import Product from '../models/Product';
 
 // Create a new product
-exports.createProduct =  function(req, res) {
+exports.createProduct = async (req, res) => {
   try {
-    var name = req.body.name;
-    var price = req.body.price;
-    var imageLink = req.body.imageLink;
+    const { name, price, imageLink } = req.body;
 
-    let newProdct = new Product({ name: name, price: price, imageLink: imageLink });
+    // Create a new product instance
+    const newProduct = new Product({ name, price, imageLink });
 
+    // Save the new product to the database
     await newProduct.save();
 
+    // Send a success response with the created product
     res.status(201).json({ message: 'Product created successfully', product: newProduct });
   } catch (error) {
+    // Log the error and send an error response
     console.error('Error creating product:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
-exports.getAllProducts = async function(req, res) {
+// Get all products
+exports.getAllProducts = async (req, res) => {
   try {
-    var products = await Product.find();
+    // Find all products in the database
+    const products = await Product.find();
 
+    // Send the products as the response
     res.json(products);
   } catch (error) {
+    // Log the error and send an error response
     console.error('Error fetching products:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
