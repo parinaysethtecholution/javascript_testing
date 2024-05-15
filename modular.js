01d09789520a8354c2c06734c0f1acde048e6f2c
Harshit Rathore <harshit.rathore@techolution.com>
2024-05-15 19:51:58 +0530

    Refactored code updated

--- modular.js
+++ modular.js
@@ -1,43 +1,44 @@
+
 export function returnChats(obj) {
-    let arr = []; 
-    Object.keys(obj).forEach(function(elem) {
-        arr.push({
-            "id": obj[elem].id,
-            "question": obj[elem].question,
-            "response": obj[elem].response,
-            "timeStamp": obj[elem].timeStamp
-        });
+  let arr = [];
+  Object.keys(obj).forEach(function (elem) {
+    arr.push({
+      id: obj[elem].id,
+      question: obj[elem].question,
+      response: obj[elem].response,
+      timeStamp: obj[elem].timeStamp,
     });
-    return arr;
+  });
+  return arr;
 }
 
 export function returnTaskChats(obj) {
-    let arr = [];
-    Object.keys(obj).forEach(function(elem) {
-        arr.push({
-            "id": obj[elem].id,
-            "question": obj[elem].question,
-            "response": obj[elem].response,
-            "taskCreated": obj[elem].taskCreated
-        });
+  let arr = [];
+  Object.keys(obj).forEach(function (elem) {
+    arr.push({
+      id: obj[elem].id,
+      question: obj[elem].question,
+      response: obj[elem].response,
+      taskCreated: obj[elem].taskCreated,
     });
-    return arr;
+  });
+  return arr;
 }
 
 export function returnCity(str) {
-    const match = str.match(/"([^"]*)"/);
-    return match ? match[1] : null;
+  const match = str.match(/("([^"]+)")/);
+  return match ? match[1] : null;
 }
 
 export function returnWeatherDetails(object) {
-    let arr = []; 
-    for (let key in object) {
-        if (object[key] && object[key] !== 0) {
-            arr.push({
-                key: key,
-                value: object[key]
-            });
-        }
+  let arr = [];
+  for (let key in object) {
+    if (object[key] && object[key] !== 0) {
+      arr.push({
+        key: key,
+        value: object[key],
+      });
     }
-    return arr;
+  }
+  return arr;
 }
