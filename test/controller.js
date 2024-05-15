feab619a3b980f641adc79a52a54bac80a701122
harshit-rathore3 <153714797+harshit-rathore3@users.noreply.github.com>
2024-05-15 20:27:23 +0530

    Update controller.js

--- test/controller.js
+++ test/controller.js
@@ -24,7 +24,7 @@ exports.getAllProducts = async function(req, res) {
 
     res.json(products);
   } catch (error) {
-    console.error('Error fetching products:', error);
+    console.error('Error fetching products:', error); 
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
