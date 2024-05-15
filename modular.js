378c4fc07ec185be8fca734ac53b0713d0d457c2
Harshit Rathore <harshit.rathore@techolution.com>
2024-05-15 12:54:30 +0000

    Refactored code updated

--- modular.js
+++ modular.js
@@ -1,45 +1,70 @@
+
+// Function to return an array of chat objects
 export function returnChats(obj) {
-    let arr = [];
-    Object.keys(obj).forEach(function(elem) {
-        arr.push({
-            "id": obj[elem].id,
-            "question": obj[elem].question,
-            "response": obj[elem].response,
-            "timeStamp": obj[elem].timeStamp
-        });
-    });
+  // Create an empty array to store chat objects
+  const chats = [];
 
-    
-    return arr;
+  // Iterate over the keys of the input object
+  Object.keys(obj).forEach((key) => {
+    // Push a new chat object to the chats array
+    chats.push({
+      id: obj[key].id,
+      question: obj[key].question,
+      response: obj[key].response,
+      timeStamp: obj[key].timeStamp,
+    });
+  });
+
+  // Return the chats array
+  return chats;
 }
 
+// Function to return an array of task chat objects
 export function returnTaskChats(obj) {
-    let arr = [];
-    Object.keys(obj).forEach(function(elem) {
-        arr.push({
-            "id": obj[elem].id,
-            "question": obj[elem].question,
-            "response": obj[elem].response,
-            "taskCreated": obj[elem].taskCreated
-        });
+  // Create an empty array to store task chat objects
+  const taskChats = [];
+
+  // Iterate over the keys of the input object
+  Object.keys(obj).forEach((key) => {
+    // Push a new task chat object to the taskChats array
+    taskChats.push({
+      id: obj[key].id,
+      question: obj[key].question,
+      response: obj[key].response,
+      taskCreated: obj[key].taskCreated,
     });
-    return arr;
+  });
+
+  // Return the taskChats array
+  return taskChats;
 }
 
+// Function to extract the city name from a string
 export function returnCity(str) {
-    const match = str.match(/"([^"]*)"/);
-    return match ? match[1] : null;
+  // Use regular expression to match the city name enclosed in double quotes
+  const match = str.match(/"([^"]*)"/) || [];
+
+  // Return the matched city name or null if no match found
+  return match[1] || null;
 }
 
+// Function to return an array of weather details
 export function returnWeatherDetails(object) {
-    let arr = [];
-    for (let key in object) {
-        if (object[key] && object[key] !== 0) {
-            arr.push({
-                key: key,
-                value: object[key]
-            });
-        }
+  // Create an empty array to store weather details
+  const weatherDetails = [];
+
+  // Iterate over the keys of the input object
+  for (const key in object) {
+    // Check if the value is truthy and not equal to 0
+    if (object[key] && object[key] !== 0) {
+      // Push a new weather detail object to the weatherDetails array
+      weatherDetails.push({
+        key,
+        value: object[key],
+      });
     }
-    return arr;
+  }
+
+  // Return the weatherDetails array
+  return weatherDetails;
 }
