32ea0d9dbbcd313ace80fb016f4e69d286503ac7
Harshit Rathore <harshit.rathore@techolution.com>
2024-05-16 14:06:05 +0000

    Refactored code updated

--- customHook.js
+++ customHook.js
@@ -1,82 +1,43 @@
-const { useEffect } = require("react");
-const { useDispatch, useSelector } = require("react-redux");
-const { apiOptions, movieVideoUrl } = require("../Constants/apiConstants");
-const { setMovieTrailer } = require("../store/moviesSlice");
-const { isEmpty } = require("lodash");
-
 
 const useFetchMovieTrailer = (movieId) => {
-    const dispatch = useDispatch();
-    const trailer = useSelector(store => store?.movie?.movieTrailer);
-    const fetchTrailer = async () => {
-        const apiData = await fetch(movieVideoUrl(movieId), apiOptions);
-        const jsonData = await apiData.json();
-        let trailers;
-        if (jsonData && jsonData.results) {
-            trailers = jsonData.results.filter((elem) => {
-                if (elem && elem.type === "Trailer") {
-                    return true;
-                }
-                return false;
-            });
-        }
-        let trailerObject;
-        if (trailers && trailers.length > 0) {
-            trailerObject = trailers[0];
-        } else {
-            trailerObject = jsonData.results[0];
-        }
-        dispatch(setMovieTrailer(trailerObject))
-    }
+  // Add comments to explain the purpose of each block of code
+  const dispatch = useDispatch();
+  const trailer = useSelector((store) => store?.movie?.movieTrailer);
 
-    useEffect(() => {
-        if (isEmpty(trailer)) {
-            fetchTrailer()
+  // Fetch the movie trailer from the API
+  const fetchTrailer = async () => {
+    const apiData = await fetch(movieVideoUrl(movieId), apiOptions);
+    const jsonData = await apiData.json();
+    let trailers;
+    if (jsonData && jsonData.results) {
+      // Filter the results to get only the trailer
+      trailers = jsonData.results.filter((elem) => {
+        if (elem && elem.type === "Trailer") {
+          return true;
         }
-    }, [])
-}
+        return false;
+      });
+    }
+    let trailerObject;
+    if (trailers && trailers.length > 0) {
+      // Use the first trailer in the list
+      trailerObject = trailers[0];
+    } else {
+      // Use the first result if no trailer is found
+      trailerObject = jsonData.results[0];
+    }
+    // Dispatch the trailer object to the store
+    dispatch(setMovieTrailer(trailerObject));
+  };
+
+  // Fetch the trailer if it's not already in the store
+  useEffect(() => {
+    if (isEmpty(trailer)) {
+      fetchTrailer();
+    }
+  }, []);
+
+  return useFetchMovieTrailer;
+};
 
 export default useFetchMovieTrailer;
-
-
-
-
-
-class ParkingLot {
-  constructor(size) {
-    this.slots = new Array(size).fill(null);
-    this.availableCount = size;
-  }
-
-  parkCar(car) {
-    if (this.availableCount === 0) return false;
-    const emptySlot = this.slots.findIndex(slot => slot === null);
-    this.slots[emptySlot] = car;
-    this.availableCount--;
-    return true;
-  }
-
-  removeCar(car) {
-    const slotIndex = this.slots.findIndex(slot => slot === car);
-    if (slotIndex === -1) return false;
-    this.slots[slotIndex] = null;
-    this.availableCount++;
-    return true;
-  }
-
-  getAvailableSlots() {
-    return this.availableCount;
-  }
-
-  isFull() {
-    return this.availableCount === 0;
-  }
-}
-
-
-const lot = new ParkingLot(10);
-lot.parkCar("CAR123");
-lot.parkCar("TRUCK789");
-console.log(lot.getAvailableSlots()); // 8
-console.log(lot.removeCar("TRUCK789")); // true
-console.log(lot.isFull()); // false
