dacfe3fbd85edd171b49e24960318c2bb4fdf3d8
parinaysethtecholution <158596517+parinaysethtecholution@users.noreply.github.com>
2024-05-17 01:58:24 +0530

    Update factorial.js
    pipelinetest

--- factorial.js
+++ factorial.js
@@ -6,9 +6,7 @@ function factorialIterative(n) {
     for (let i = 1; i <= n; i++) {
         result *= i;
     }
-    return result;
-}
-
+    return result
 console.log(factorialIterative(5)); // Output: 120
 
 
