e9560239b70b1100ef7040418e7dd2919fe59a49
Harshit Rathore <harshit.rathore@techolution.com>
2024-05-20 17:02:47 +0530

    Refactored code updated

--- modular.js
+++ modular.js
@@ -1,43 +1,58 @@
+
+// Function to return an array of chat objects
 export function returnChats(obj) {
-    let arr = []; 
-    Object.keys(obj).forEach(function(elem) {
-        arr.push({
-            "id": obj[elem].id,
-            "question": obj[elem].question,
-            "response": obj[elem].response,
-            "timeStamp": obj[elem].timeStamp
-        });
+  const arr = [];
+
+  // Iterate over the keys of the input object
+  Object.keys(obj).forEach(function(key) {
+    arr.push({
+      "id": obj[key].id,
+      "question": obj[key].question,
+      "response": obj[key].response,
+      "timeStamp": obj[key].timeStamp
     });
-    return arr;
+  });
+
+  return arr;
 }
 
+// Function to return an array of task chat objects
 export function returnTaskChats(obj) {
-    let arr = [];
-    Object.keys(obj).forEach(function(elem) {
-        arr.push({
-            "id": obj[elem].id,
-            "question": obj[elem].question,
-            "response": obj[elem].response,
-            "taskCreated": obj[elem].taskCreated
-        });
+  const arr = [];
+
+  // Iterate over the keys of the input object
+  Object.keys(obj).forEach(function(key) {
+    arr.push({
+      "id": obj[key].id,
+      "question": obj[key].question,
+      "response": obj[key].response,
+      "taskCreated": obj[key].taskCreated
     });
-    return arr;
+  });
+
+  return arr;
 }
 
+// Function to extract the city name from a string
 export function returnCity(str) {
-    const match = str.match(/"([^"]*)"/);
-    return match ? match[1] : null;
+  const match = str.match(/"([^"]*)"/) // Regular expression to match a quoted string
+  return match ? match[1] : null; // Return the matched string or null if no match
 }
 
+// Function to return an array of key-value pairs from an object
 export function returnWeatherDetails(object) {
-    let arr = []; 
-    for (let key in object) {
-        if (object[key] && object[key] !== 0) {
-            arr.push({
-                key: key,
-                value: object[key]
-            });
-        }
+  const arr = [];
+
+  // Iterate over the keys of the input object
+  for (let key in object) {
+    // Check if the value is truthy and not equal to 0
+    if (object[key] && object[key] !== 0) {
+      arr.push({
+        key: key,
+        value: object[key]
+      });
     }
-    return arr;
+  }
+
+  return arr;
 }
