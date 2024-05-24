62397f2eda7d6e4a7a9a6f1659ce303e748fce90
Nikhil-Guru-NG <nikhil.venkatesh@techolution.com>
2024-05-24 16:44:22 +0530

    Update controller_ref.js

--- controller_ref.js
+++ controller_ref.js
@@ -38,3 +38,10 @@ exports.getAllProducts = async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
+export function returnBogus(object) {
+    let arr = []; // Initialize an empty array
+    for(let key in object) { // Iterate over each key in the object
+        console.log("This is Bogus"); // Log a message to the console
+    }
+}
