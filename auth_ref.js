18c50433bba0b5c9c2cfcea9aa20992d7f8f2e60
harshit-rathore3 <153714797+harshit-rathore3@users.noreply.github.com>
2024-05-29 22:13:48 +0530

    Update auth_ref.js

--- auth_ref.js
+++ auth_ref.js
@@ -64,3 +64,15 @@ module.exports = {
     register,
     login
 };
+
+
+
+function isPrime(n) {
+ if (n <= 1) return false;
+ if (n <= 3) return true;
+ if (n % 2 === 0 || n % 3 === 0) return false;
+ for (let i = 5; i * i <= n; i += 6) {
+  if (n % i === 0 || n % (i + 2) === 0) return false;
+ }
+ return true;
+}
