657e803852ff5d19234fe46afd19e594f79c2195
harshit-rathore3 <harshit.rathore@techolution.com>
2024-07-02 11:16:01 +0530

    Refactored code updated

--- modular.js
+++ modular.js
@@ -1,43 +1,51 @@
+
+// utils.js
+
+/**
+ * Converts an object to an array of objects with specific properties.
+ * @param {Object} obj - The input object.
+ * @returns {Array} An array of objects with properties 'id', 'question', 'response', and 'timeStamp'.
+ */
 export function returnChats(obj) {
-    let arr = []; 
-    Object.keys(obj).forEach(function(elem) {
-        arr.push({
-            "id": obj[elem].id,
-            "question": obj[elem].question,
-            "response": obj[elem].response,
-            "timeStamp": obj[elem].timeStamp
-        });
-    });
-    return arr;
+  return Object.values(obj).map(({ id, question, response, timeStamp }) => ({
+    id,
+    question,
+    response,
+    timeStamp,
+  }));
 }
 
+/**
+ * Converts an object to an array of objects with specific properties.
+ * @param {Object} obj - The input object.
+ * @returns {Array} An array of objects with properties 'id', 'question', 'response', and 'taskCreated'.
+ */
 export function returnTaskChats(obj) {
-    let arr = [];
-    Object.keys(obj).forEach(function(elem) {
-        arr.push({
-            "id": obj[elem].id,
-            "question": obj[elem].question,
-            "response": obj[elem].response,
-            "taskCreated": obj[elem].taskCreated
-        });
-    });
-    return arr;
+  return Object.values(obj).map(({ id, question, response, taskCreated }) => ({
+    id,
+    question,
+    response,
+    taskCreated,
+  }));
 }
 
+/**
+ * Extracts the city name from a given string.
+ * @param {string} str - The input string.
+ * @returns {string|null} The city name if found, otherwise null.
+ */
 export function returnCity(str) {
-    const match = str.match(/"([^"]*)"/);
-    return match ? match[1] : null;
+  const match = str.match(/"([^"]*)"/);
+  return match ? match[1] : null;
 }
 
+/**
+ * Converts an object to an array of objects with 'key' and 'value' properties.
+ * @param {Object} object - The input object.
+ * @returns {Array} An array of objects with properties 'key' and 'value'.
+ */
 export function returnWeatherDetails(object) {
-    let arr = []; 
-    for (let key in object) {
-        if (object[key] && object[key] !== 0) {
-            arr.push({
-                key: key,
-                value: object[key]
-            });
-        }
-    }
-    return arr;
+  return Object.entries(object)
+    .filter(([, value]) => value !== 0)
+    .map(([key, value]) => ({ key, value }));
 }
