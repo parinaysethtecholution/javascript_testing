66fb4e1fae62a6ce3b04897f2226cd1b19dc6e46
harshit-rathore3 <harshit.rathore@techolution.com>
2024-07-02 08:23:18 +0000

    Refactored code updated

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -1,13 +1,30 @@
+
+// ShoppingCart.js
+
+/**
+ * Class representing a shopping cart.
+ */
 class ShoppingCart {
+  /**
+   * Creates a new instance of the ShoppingCart class.
+   */
   constructor() {
     this.items = [];
     this.discount = 0;
   }
 
+  /**
+   * Adds an item to the shopping cart.
+   * @param {Object} item - The item to be added.
+   */
   addItem(item) {
     this.items.push(item);
   }
 
+  /**
+   * Removes an item from the shopping cart by index.
+   * @param {number} index - The index of the item to be removed.
+   */
   removeItem(index) {
     if (index >= 0 && index < this.items.length) {
       this.items.splice(index, 1);
@@ -16,6 +33,10 @@ class ShoppingCart {
     }
   }
 
+  /**
+   * Calculates and returns the total price of all items in the shopping cart after applying the discount.
+   * @returns {number} The total price of all items after applying the discount.
+   */
   getTotalPrice() {
     let totalPrice = 0;
     for (const item of this.items) {
@@ -24,10 +45,17 @@ class ShoppingCart {
     return totalPrice - this.discount;
   }
 
+  /**
+   * Applies a discount to the shopping cart.
+   * @param {number} amount - The discount amount to be applied.
+   */
   applyDiscount(amount) {
     this.discount = amount;
   }
 
+  /**
+   * Displays all items in the shopping cart with their index, name, and price.
+   */
   displayItems() {
     console.log('Shopping Cart Items:');
     this.items.forEach((item, index) => {
@@ -35,25 +63,46 @@ class ShoppingCart {
     });
   }
 
+  /**
+   * Clears the shopping cart by removing all items and resetting the discount.
+   */
   clearCart() {
     this.items = [];
     this.discount = 0;
     console.log('Cart cleared.');
   }
 
+  /**
+   * Returns the number of items in the shopping cart.
+   * @returns {number} The number of items in the shopping cart.
+   */
   getItemCount() {
     return this.items.length;
   }
 
+  /**
+   * Finds and returns the first item in the shopping cart with the specified name.
+   * @param {string} name - The name of the item to find.
+   * @returns {Object|undefined} The item object if found, otherwise undefined.
+   */
   getItemByName(name) {
     return this.items.find(item => item.name === name);
   }
 
+  /**
+   * Checks if the shopping cart contains an item with the specified name.
+   * @param {string} name - The name of the item to check for.
+   * @returns {boolean} True if the item is found, false otherwise.
+   */
   containsItem(name) {
     return this.items.some(item => item.name === name);
   }
 }
 
-// Comment
-
-
+// Usage example
+const cart = new ShoppingCart();
+cart.addItem({ name: 'T-Shirt', price: 19.99 });
+cart.addItem({ name: 'Jeans', price: 39.99 });
+cart.applyDiscount(10);
+cart.displayItems();
+console.log(`Total price: $${cart.getTotalPrice()}`);
